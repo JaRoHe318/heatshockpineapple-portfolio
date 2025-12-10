be2b256a53dca96a8ab488006cddd80742ffb23b
JaRoHe318 <jarohe1000@gmail.com>
2025-12-09 23:47:36 -0800

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://heatshockpineapple.com',
+  // *** ADD THIS NEW LINE ***
+  base: '/heatshockpineapple-portfolio/', 
+  // *************************
   output: 'static',
   build: {
     format: 'file'
